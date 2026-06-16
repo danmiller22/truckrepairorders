79491ec6ca8ecb2d049f72114110b1ed6b9bbf8c
Mueller <ajdarn3@gmail.com>
2026-06-16 16:28:56 -0500

    Update utils.ts

--- utils.ts
+++ utils.ts
@@ -5,11 +5,3 @@ export async function sendMessage(token: string, chat_id: string, text: string, 
     body: JSON.stringify({ chat_id, text, reply_markup })
   });
 }
-
-export async function sendPhoto(token: string, chat_id: string, photo: string, caption?: string) {
-  await fetch(`https://api.telegram.org/bot${token}/sendPhoto`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ chat_id, photo, caption })
-  });
-}
